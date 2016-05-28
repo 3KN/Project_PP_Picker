354b74ed492a691633b53520b3d1a6e66bceb23f
Alex Buzevych <alex@mail.ru>
2016-05-28 12:04:54 +0300

    Added tabbed

--- app/src/main/java/com/example/alex/test/fragments/Arena_Fragment.java
+++ app/src/main/java/com/example/alex/test/fragments/Arena_Fragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.alex.test.R;
 
@@ -24,7 +25,10 @@ public class Arena_Fragment extends android.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_arena_, container, false);
+
     }
 
 }
